6e0aac49124744895e133f7f22f1f4dd4ef9c2e6
serj1chen <s.chenakal@gmail.com>
2014-11-08 23:23:24 +0200

    api

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ uawebchallenge hackaton
 
 
 http://yiiframework.ru 1.1.15
+
+[
+   {
+      "id":2,
+      "method":"ProductCategory.getList",
+      "params":{
+         "data":{
+            "name":"vasya"
+         },
+         "key":"somekey"
+      }
+   },
+   {
+      "id":5,
+      "jsonrpc":"2.0",
+      "key":555555,
+      "method":"Class.method",
+      "params":{
+         "data":{
+            "name":"vasya"
+         },
+         "key":"fhdjfjdhfjhdfhdjfhd"
+      }
+   }
+]
+
--- app/protected/controllers/TestController.php
+++ app/protected/controllers/TestController.php
@@ -93,6 +93,7 @@ class TestController extends Controller
 
         $model2 = new stdClass();
         $model2->id = 5;
+        $model2->jsonrpc = '2.0';
         $model2->key = 555555;
         $model2->method = 'Class.method';
         $model2->params = array();
@@ -104,6 +105,42 @@ class TestController extends Controller
         $data[] = $model2;
 
         $data =  json_encode($data);
+        var_dump($data);
+        echo '<br>';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://localhost/app/?format=jsonrpc2&data=' . $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml"));
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'dfdfdfdfdfdf');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_PROXY, null);
+
+        $response = curl_exec($ch);
+
+        var_dump($response);
+
+
+    }
+
+    public function actionChen3()
+    {
+
+        $data = array();
+
+        $model = new stdClass();
+        $model->id = 2;
+        $model->method = 'ProductCategory.getList';
+        $model->params = array();
+        $model->params['data'] = array(
+            'name' => 'vasya',
+        );
+        $model->params['key'] = 'somekey';
+
+        $data =  json_encode($model);
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'http://localhost/app/?format=jsonrpc2&data=' . $data);
