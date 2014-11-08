a74b6d69567fe4e2cf71736947bb19f8f8c25b7e
serj1chen <s.chenakal@gmail.com>
2014-11-08 22:34:08 +0200

    api

--- app/protected/components/api/Validators/ProductCategory.php
+++ app/protected/components/api/Validators/ProductCategory.php
@@ -9,14 +9,7 @@
 class ProductCategory extends BaseValidator {
 
     public function getList($data){
-//        $this->ValidationMap = array(
-//
-//        );
-        if ($this->validate($data)){
-            return $this->Model->getList($data);
-        } else {
-            return $this;
-        }
+        return $this->Model->getList($data);
     }
 
 } 
