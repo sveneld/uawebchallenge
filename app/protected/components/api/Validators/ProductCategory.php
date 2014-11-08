04cd2d0434053089ea23f9c96134980644ea2e8e
sg552 <galaevnik@yandex.ru>
2014-11-08 22:01:40 +0200

    DCR as Object

--- app/protected/components/api/Validators/ProductCategory.php
+++ app/protected/components/api/Validators/ProductCategory.php
@@ -9,9 +9,9 @@
 class ProductCategory extends BaseValidator {
 
     public function getList($data){
-        $this->ValidationMap = array(
-
-        );
+//        $this->ValidationMap = array(
+//
+//        );
         if ($this->validate($data)){
             return $this->Model->getList($data);
         } else {
