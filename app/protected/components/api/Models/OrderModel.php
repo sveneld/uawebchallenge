8735bdbd51d3e77f3a88a0cbddf2b1cf16b6e49d
sveneld <sveneld@mail.ru>
2014-11-08 19:52:45 +0200

    ApiKey in orders

--- app/protected/components/api/Models/OrderModel.php
+++ app/protected/components/api/Models/OrderModel.php
@@ -1,32 +1,104 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Sveneld
  * Date: 08.11.14
  * Time: 16:16
  */
+class OrderModel extends DataContainerResponse
+{
 
-class OrderModel {
+    public function add($data)
+    {
+        $order = new YOrder();
+        $order->FullName = $data->FullName;
+        $order->Phone = $data->Phone;
+        $order->PhoneAdditional = $data->PhoneAdditional;
+        $order->Country = $data->Country;
+        $order->City = $data->City;
+        $order->Address = $data->Address;
+        $order->AddressAdditional = $data->AddressAdditional;
 
-    public function add(){
+        $criteria = new CDbCriteria();
+        $products = array();
+        foreach ($data->Products as $product) {
+            $products[] = $product->Sku;
+        }
+        $products = $criteria->addInCondition('Sku', $products);
+        $groupedProducts = array();
+        foreach ($products as $product) {
+            $groupedProducts[$product->Sku] = $product;
+        }
+        $subTotal = 0;
+        foreach ($data->Products as $product) {
+            $subTotal += $groupedProducts[$product->Sku]->Price * $product->Quantity;
+        }
+
+        $order->SubTotal = $subTotal;
+
+        $shippingMethod = YShippingMethod::model()->findByPK($data->IdShippingMethod);
+        $order->ShippingTotal = $shippingMethod->Cost;
+        $order->ShippingMethodName = $shippingMethod->Name;
+        $order->IdShippingMethod = $data->IdShippingMethod;
+
+        $paymentMethod = YPaymentMethod::model()->findByPK($data->IdPaymentMethod);
+        $order->PaymentTotal = $paymentMethod->Cost;
+        $order->PaymentMethodName = $paymentMethod->Name;
+        $order->IdPaymentMethod = $data->IdPaymentMethod;
+
+        $order->Discount = $data->Discount;
+        $order->Fee = $data->Fee;
+        $order->Total = $data->Total;
+
+        $order->PaymentMethodName = $data->PaymentMethodName;
+        $order->IdPaymentMethod = $data->IdPaymentMethod;
+        $order->IdOrderStatus = 1;
+
+        if (!$order->save()) {
+            return $this->addError('Fatal error 001');
+        }
+
+        foreach ($data->Products as $product) {
+            $orderProduct = new YOrderProduct();
+            $orderProduct->IdOrder = $order->Id;
+            $orderProduct->IdProduct = $groupedProducts[$product->Sku]->Id;
+            $orderProduct->Sku = $product->Sku;
+            $orderProduct->Name = $groupedProducts[$product->Sku]->Name;
+            $orderProduct->Price = $product->Price;
+            $orderProduct->Quantity = $product->Quantity;
+            $orderProduct->IncomingPrice = $groupedProducts[$product->Sku]->IncomingPrice;
+            $orderProduct->IncomingCurrency = $groupedProducts[$product->Sku]->IncomingCurrency;
+            $orderProduct->IncomingCurrencyRate = $groupedProducts[$product->Sku]->IncomingCurrencyRate;
+
+            if (!$order->save()) {
+                return $this->addError('Fatal error 002');
+            }
+
+        }
+
+        $data = new stdClass();
+        $data->IdOrder = $order->Id;;
+        $this->addData($data);
+        return $this;
+    }
+
+    public function get()
+    {
         $data = new stdClass();
         $data->SomeVal = 'coool555';
         $this->addData($data);
     }
 
-    public function get(){
+    public function getList()
+    {
         $data = new stdClass();
         $data->SomeVal = 'coool555';
         $this->addData($data);
     }
 
-    public function getList(){
-        $data = new stdClass();
-        $data->SomeVal = 'coool555';
-        $this->addData($data);
-    }
-
-    public function getStatus(){
+    public function getStatus()
+    {
         $data = new stdClass();
         $data->SomeVal = 'coool555';
         $this->addData($data);
--- app/protected/models/YOrderProduct.php
+++ app/protected/models/YOrderProduct.php
@@ -10,100 +10,109 @@
  * @property string $Name
  * @property string $Quantity
  * @property string $Price
+ * @property string $IncomingPrice
+ * @property string $IncomingCurrency
+ * @property string $IncomingCurrencyRate
  */
 class YOrderProduct extends CActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'order_products';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'order_products';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('IdOrder, IdProduct', 'required'),
-			array('IdOrder, IdProduct, Quantity', 'length', 'max'=>10),
-			array('Sku, Name', 'length', 'max'=>255),
-			array('Price', 'length', 'max'=>15),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('IdOrder, IdProduct, Sku, Name, Quantity, Price', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('IdOrder, IdProduct', 'required'),
+            array('IdOrder, IdProduct, Quantity', 'length', 'max' => 10),
+            array('Sku, Name', 'length', 'max' => 255),
+            array('Price', 'length', 'max' => 15),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('IdOrder, IdProduct, Sku, Name, Quantity, Price, IncomingPrice, IncomingCurrency, IncomingCurrencyRate ', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
             'Product' => array(self::BELONGS_TO, 'YOrder', array('IdOrder' => 'Id')),
-		);
-	}
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'IdOrder' => 'Id Order',
-			'IdProduct' => 'Id Product',
-			'Sku' => 'Sku',
-			'Name' => 'Name',
-			'Quantity' => 'Quantity',
-			'Price' => 'Price',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'IdOrder' => 'Id Order',
+            'IdProduct' => 'Id Product',
+            'Sku' => 'Sku',
+            'Name' => 'Name',
+            'Quantity' => 'Quantity',
+            'Price' => 'Price',
+            'IncomingPrice' => 'Incoming Price',
+            'IncomingCurrency' => 'Incoming Currency',
+            'IncomingCurrencyRate' => 'Incoming Currency Rate',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('IdOrder',$this->IdOrder,true);
-		$criteria->compare('IdProduct',$this->IdProduct,true);
-		$criteria->compare('Sku',$this->Sku,true);
-		$criteria->compare('Name',$this->Name,true);
-		$criteria->compare('Quantity',$this->Quantity,true);
-		$criteria->compare('Price',$this->Price,true);
+        $criteria->compare('IdOrder', $this->IdOrder, true);
+        $criteria->compare('IdProduct', $this->IdProduct, true);
+        $criteria->compare('Sku', $this->Sku, true);
+        $criteria->compare('Name', $this->Name, true);
+        $criteria->compare('Quantity', $this->Quantity, true);
+        $criteria->compare('Price', $this->Price, true);
+        $criteria->compare('IncomingPrice', $this->IncomingPrice, true);
+        $criteria->compare('IncomingCurrency', $this->IncomingCurrency, true);
+        $criteria->compare('IncomingCurrencyRate', $this->IncomingCurrencyRate, true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return OrderProducts the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return OrderProducts the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 }
