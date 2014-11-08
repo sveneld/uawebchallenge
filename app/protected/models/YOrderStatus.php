39ea6a5807d7f0919ed7ba8e7d2f4d43bb47aaa5
sveneld <sveneld@mail.ru>
2014-11-08 15:31:38 +0200

    yii models

--- app/protected/models/YOrderStatus.php
+++ app/protected/models/YOrderStatus.php
@@ -42,6 +42,7 @@ class YOrderStatus extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+            'OrderStatus' => array(self::BELONGS_TO, 'YOrder', array('Id' => 'IdOrderStatus')),
 		);
 	}
 
