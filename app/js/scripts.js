2e205a4695a8cc19a0184538aef0ab49bd745a4d
serj1chen <s.chenakal@gmail.com>
2014-11-09 09:22:44 +0200

    api

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,5 +1,7 @@
 
 $(function(){
-    var SendRequestForm_request = $('#SendRequestForm_request');
-    SendRequestForm_request.val(JSON.stringify(SendRequestForm_request.val(), null, 4));
+    //var SendRequestForm_request = $('#SendRequestForm_request');
+    //SendRequestForm_request.val(JSON.stringify(SendRequestForm_request.val(), null, 4));
+    //alert(5555)
 });
+
