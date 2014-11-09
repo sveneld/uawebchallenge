05b5dc4df697c3539cec10eaeabed1c0cacb2183
serj1chen <s.chenakal@gmail.com>
2014-11-09 09:36:13 +0200

    api

--- app/protected/models/SendRequestForm.php
+++ app/protected/models/SendRequestForm.php
@@ -2,7 +2,7 @@
 
 class SendRequestForm extends CFormModel
 {
-    public $request = '{"jsonrpc": "2.0", "method": "subtract", "params": "data", "id": 1}';
+    public $request = '{"jsonrpc": "2.0", "method": "class.method", "params": "data", "id": 1}';
     public $response;
 
     public function attributeLabels()
