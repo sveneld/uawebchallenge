f0032c62444479c34a48030c5b3d787d150021d7
serj1chen <s.chenakal@gmail.com>
2014-11-09 08:32:50 +0200

    api

--- app/index.php
+++ app/index.php
@@ -8,6 +8,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+define('CACHE_LIFETIME', 36000);
 
 require_once($yii);
 Yii::$enableIncludePath = false;
@@ -17,4 +18,3 @@ function dump($msg, $die = 0){
     VarDumper::dump($msg, $die);
 }
 
-CONST CACHE_LIFETIME = 36000;
