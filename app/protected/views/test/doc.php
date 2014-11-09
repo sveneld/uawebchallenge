0d1e4548100b90902fcc1c703c6689ab363b7c27
serj1chen <s.chenakal@gmail.com>
2014-11-09 10:07:45 +0200

    api

--- app/protected/views/test/doc.php
+++ app/protected/views/test/doc.php
@@ -16,10 +16,59 @@
     <tr>
         <td>
             Отримати список товарів
-            <pre></pre>
+            <pre>{
+    "jsonrpc": "2.0",
+    "method": "Product.getList",
+    "params": {
+        "data": null,
+        "key": "03ae7344fce64b8ef0c7dc3a78fae838"
+    },
+    "id": 1
+}</pre>
         </td>
         <td>
-            <pre></pre>
+            <pre>{
+    "jsonrpc": "2.0",
+    "id": 1,
+    "result": [
+        {
+            "Id": "4972",
+            "Sku": "FB263",
+            "Name": "deathnote ryuk фигурка",
+            "Description": "Высота ~20 см",
+            "Image": "1241695668.jpg",
+            "InStock": "0",
+            "Price": "225.0000"
+        },
+        {
+            "Id": "4973",
+            "Sku": "FB264",
+            "Name": "code geass c.c фигурка",
+            "Description": "-",
+            "Image": "1243256060.jpg",
+            "InStock": "0",
+            "Price": "302.0000"
+        },
+        {
+            "Id": "4975",
+            "Sku": "FB266",
+            "Name": "kuroshitsuji Sebastian фигурка",
+            "Description": "Высота ~20 см",
+            "Image": "1244805796.jpg",
+            "InStock": "0",
+            "Price": "285.0000"
+        },
+        {
+            "Id": "4976",
+            "Sku": "FB267",
+            "Name": "kuroshitsuji Ciel фигурка",
+            "Description": "Высота ~20 см",
+            "Image": "1245587468.jpg",
+            "InStock": "0",
+            "Price": "299.0000"
+        }...
+    ]
+}</pre>
         </td>
     </tr>
     <tr>
@@ -46,16 +95,16 @@
         </td>
         <td>
             <pre>{
-   "jsonrpc":"2.0",
-   "id":1,
-   "result":[
-      {
-         "Id":"1",
-         "Name":"\u0421\u0430\u043c\u043e\u0432\u044b\u0432\u043e\u0437",
-         "Cost":"0.0000",
-         "AdditionalParam":null
-      }
-   ]
+    "jsonrpc": "2.0",
+    "id": 1,
+    "result": [
+        {
+            "Id": "1",
+            "Name": "Самовывоз",
+            "Cost": "0.0000",
+            "AdditionalParam": null
+        }
+    ]
 }</pre>
         </td>
     </tr>
