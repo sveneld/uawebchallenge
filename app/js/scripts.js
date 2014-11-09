38a3a49261898bd425e64b7e1c993a3b9ff26564
serj1chen <s.chenakal@gmail.com>
2014-11-09 09:07:26 +0200

    api

---
+++ app/js/scripts.js
@@ -0,0 +1,9 @@
+
+//$(function(){
+//    var SendRequestForm_request = $('#SendRequestForm_request');
+//    SendRequestForm_request.val(JSON.stringify(SendRequestForm_request.val(), null, 4));
+//alert(555)
+//});
+
+
+alert(555)
--- app/protected/views/layouts/main.php
+++ app/protected/views/layouts/main.php
@@ -15,6 +15,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/scripts.js"></script>
 <!--	<title>--><?php //echo CHtml::encode($this->pageTitle); ?><!--</title>-->
 </head>
 
