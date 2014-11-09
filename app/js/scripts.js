66b9b46287ab8d419f10935f4bf28f579a42e2a4
sveneld <sveneld@mail.ru>
2014-11-09 09:11:51 +0200

    js beuatify

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,9 +1,5 @@
 
-//$(function(){
-//    var SendRequestForm_request = $('#SendRequestForm_request');
-//    SendRequestForm_request.val(JSON.stringify(SendRequestForm_request.val(), null, 4));
-//alert(555)
-//});
-
-
-alert(555)
+$(function(){
+    var SendRequestForm_request = $('#SendRequestForm_request');
+    SendRequestForm_request.val(JSON.stringify(SendRequestForm_request.val(), null, 4));
+});
