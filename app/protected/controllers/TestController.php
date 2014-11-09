bb7f4d5ec138db0ae4563b7f0a413a3e5412ac63
sveneld <sveneld@mail.ru>
2014-11-09 09:03:13 +0200

    fix form submit

--- app/protected/controllers/TestController.php
+++ app/protected/controllers/TestController.php
@@ -6,13 +6,11 @@ class TestController extends Controller
     public function actionIndex()
     {
         $model = new SendRequestForm();
-        if(isset($_POST['SendRequestForm']))
+        if(isset($_POST['SendRequestForm'])){
             $model->attributes=$_POST['SendRequestForm'];
-        if($model->validate())
-            $model->send();
-
-
-
+            if($model->validate())
+                $model->send();
+        }
         $this->render('test', array('model'=>$model));
     }
 
